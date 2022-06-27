4425996c04d4c3cd7af28bf5b1f20d9266c48617
Robert <bobbyruelas@gmail.com>
2022-06-26 23:23:00 -0500

    Current date now appears on index

--- Assets/script.js
+++ Assets/script.js
@@ -1,65 +1,42 @@
-var currentDay = document.querySelector("#currentTime");
-var currentTime = moment();
+var today = moment();
 
-currentDay.textContent = currentTime.format('MMM d, YYYY, h:mm:ss a');
+// Moment.js function attemtping to add click event
+$(document).ready(function(){
+    $("#currentDay").text(moment().format("dddd, MMMM YYYY"));
+    // $("#currentHour").text(moment().format("h:mm:ss a")); 
+    $(".saveBtn").on("click", function(){
+        var textArea = $(this).siblings(".description").val();
+        var time = $(this).parent().attr("id");
+        localStorage.setItem(time, textArea);
+    });
+    
+function timeChange(){
+var currentHour = moment().hour();
+        
+    $('.time-block').each(function(){
+        var idTime = parseInt($(this).attr("id"));
+        if (idTime < currentHour){
+            $(this).addClass("past");
+        } else if (idTime === currentHour){
+            $(this).addClass("present");
+            $(this).removeClass("past");
+        } else {$(this).addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            }
+        })
+    }
+timeChange();
+var timeInterval = setInterval(timeChange, 1000);
+});    
 
-// var currentDay = moment().format('dddd') + '' + moment().format('Do MMM YYYY');
-// var currentHour = moment().format('h:mm:ss a');
-// let hour = moment().hours();
-// var userInput;
-// var hourspan;   
-// var moment = function timeStamp() {
-//     let today = moment();
-//     $("#currentDay").datepicker(timeStamp.format('MMMM Do YYYY, h:mm:ss a'));
-
-//     let now = moment().format("kk");
-//     for (let i = 0; i < scheduleArray.length; i++) {
-//         scheduleArray[i].removeClass("future past present");
-
-//         if (now > scheduleArray[i].data("hour")) {
-//             scheduleArray[i].addClass("past");
-//         } else if (now === scheduleArray[i].contatinerEl("data-hour")) {
-//             scheduleArray[i].addClass("present");
-//         } else {
-//             scheduleArray[i].addClass("future");
-//         }    
-//     }
-// } 
-
-
-
-// Time elements for.textarea
-let contatinerEl = $(".contatiner");
-let time9am = $("#9am");
-let time10am = $("#10am");
-let time11am = $("#11am");
-let time12pm = $("#9pm");
-let time1pm = $("#1pm");
-let time2pm = $("#2pm");
-let time3pm = $("#3pm");
-let time4pm = $("#4pm");
-let time5pm = $("#5pm");
-
-let scheduleArray = [
-    time9am,
-    time10am,
-    time11am,
-    time12pm,
-    time1pm,
-    time2pm,
-    time3pm,
-    time4pm,
-    time5pm,
-];
-
-// Provides function for click events
-function formSubmit(event) {
-    event.preventDefault();
-
-    let btnClicked = $(event.currentTarget);
-    let targetText = btnClicked.siblings("textarea");
-    let targetTime = targetText.data("hour");
-    localStorage.setItem("time" + targetTime, targetText.val());
-}
-
-saveBtn.on("click", formSubmit);
+// Time elements for .textarea attempting to add local storage //
+    $("#9am .description").val(localStorage.getItem("9am"));
+    $("#10am .description").val(localStorage.getItem("10am"));
+    $("#11am .description").val(localStorage.getItem("11am"));
+    $("#12am .description").val(localStorage.getItem("12pm"));
+    $("#1pm .description").val(localStorage.getItem("1pm"));
+    $("#2pm .description").val(localStorage.getItem("2pm"));
+    $("#3pm .description").val(localStorage.getItem("3pm"));
+    $("#4pm .description").val(localStorage.getItem("4pm"));
+    $("#5pm .description").val(localStorage.getItem("5pm"));
