5b2cfbe560b13abf454cda2929e50f96820d49f2
Robert <bobbyruelas@gmail.com>
2022-06-29 20:45:26 -0500

    script and html changes for button

--- Assets/script.js
+++ Assets/script.js
@@ -5,11 +5,11 @@ $(document).ready(function(){
     $("#currentDay").text(moment().format("dddd, MMMM YYYY"));
     // $("#currentHour").text(moment().format("h:mm:ss a")); 
     $(".saveBtn").on("click", function(){
-        var textArea = $(this).siblings(".description").val();
+        var textArea = $(this).siblings(".textArea").val();
         var time = $(this).parent().attr("id");
         localStorage.setItem(time, textArea);
     });
-    
+
 function timeChange(){
 var currentHour = moment().hour();
         
@@ -28,15 +28,26 @@ var currentHour = moment().hour();
     }
 timeChange();
 var timeInterval = setInterval(timeChange, 1000);
-});    
+}); 
 
+// var today = function() {
+//     today = JSON.parse(localStorage.getItem("today"));
+//     if (!tasks) {
+//         tasks = {
+//           toDo: [],
+//           inProgress: [],
+//           inReview: [],
+//           done: []
+//         };
+//       }
+// }
 // Time elements for .textarea attempting to add local storage //
-    $("#9am .description").val(localStorage.getItem("9am"));
-    $("#10am .description").val(localStorage.getItem("10am"));
-    $("#11am .description").val(localStorage.getItem("11am"));
-    $("#12pm .description").val(localStorage.getItem("12pm"));
-    $("#1pm .description").val(localStorage.getItem("1pm"));
-    $("#2pm .description").val(localStorage.getItem("2pm"));
-    $("#3pm .description").val(localStorage.getItem("3pm"));
-    $("#4pm .description").val(localStorage.getItem("4pm"));
-    $("#5pm .description").val(localStorage.getItem("5pm"));
+    $("#9am").val(localStorage.getItem("9am"));
+    $("#10am").val(localStorage.getItem("10am"));
+    $("#11am").val(localStorage.getItem("11am"));
+    $("#12pm").val(localStorage.getItem("12pm"));
+    $("#1pm").val(localStorage.getItem("1pm"));
+    $("#2pm").val(localStorage.getItem("2pm"));
+    $("#3pm").val(localStorage.getItem("3pm"));
+    $("#4pm").val(localStorage.getItem("4pm"));
+    $("#5pm").val(localStorage.getItem("5pm"));
